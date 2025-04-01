e034af31b31cf51d96f2b4b225fee09eb6945c4d
Piotrek Budzowski <piobudzows@gmail.com>
2025-04-01 11:18:37 +0200

    Create Prueba1.java

---
+++ Prueba1.java
@@ -0,0 +1,9 @@
+package CursoJava_InterfacesGraficas;
+
+import javax.swing.JOptionPane;
+
+public class Prueba1 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola, este es un mensaje simple!");
+    }
+}
