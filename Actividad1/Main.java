19f3910a8de90b3372b128a7c40f83ba51a75caa
Piotrek Budzowski <piobudzows@gmail.com>
2025-04-06 20:38:50 +0200

    Update Main.java

--- Actividad1/Main.java
+++ Actividad1/Main.java
@@ -20,116 +20,137 @@ public class Main {
                     "3. Salir";
             int seleccion = Integer.parseInt(JOptionPane.showInputDialog(menu));
 
-            if (seleccion == 1) {
-                String nombre = pedirEntrada("Ingrese el nombre del cliente:");
-                if (nombre == null)
-                    return;
+            switch (seleccion) {
+                case 1:
+                    // CREAMOS CLIENTE LLAMANDO METODO CREARCLIENTE
+                    Cliente cliente = crearCliente();
+                    if (cliente == null)
+                        continue; // Si no se creó el cliente, volvemos al menú
 
-                String apellido = pedirEntrada("Ingrese el apellido del cliente:");
-                if (apellido == null)
-                    return;
-
-                String documento = pedirEntrada("Ingrese el documento del cliente:");
-                if (documento == null)
-                    return;
-
-                // VAMOS A COMPROBAR SI EL DOCUMENTO ES VALIDO
-                String regex = "^\\d{8}[a-zA-Z]$";
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(documento);
-                if (!matcher.matches()) {
-                    JOptionPane.showMessageDialog(null, "El DNI no es válido.");
-                    continue;
-                }
-
-                String numeroTarjeta = pedirEntrada(
-                        "Ingrese el numero de cuenta bancaria (formato: xxxx-xxxx-xxxxxxxx):");
-                if (numeroTarjeta == null)
-                    return;
-
-                // VAMOS A COMPROBAR SI NUMERO DE TARJETA ESTA BIEN
-                regex = "^(\\d{4}[-\\s]?){3}\\d{4}$";
-                pattern = Pattern.compile(regex);
-                matcher = pattern.matcher(numeroTarjeta);
-                if (!matcher.matches()) {
-                    JOptionPane.showMessageDialog(null, "La cuenta bancaria no esta correcta.");
-                    continue;
-                }
-
-                // CREAMOS EL OBJETO CLIENTE
-                Cliente cliente = new Cliente(nombre, apellido, documento, numeroTarjeta);
-
-                try {
-                    String numeroHabitacionString = pedirEntrada("Ingrese el numero de la habitacion (1-10):");
-                    if (numeroHabitacionString == null)
-                        return;
-
-                    // SE PARCEA STRING A INT
-                    int numeroHabitacion = Integer.parseInt(numeroHabitacionString);
-
-                    // COMPROBAMOS SI NUMERO DE HABITACION EXISTE
-                    if (numeroHabitacion < 1 || numeroHabitacion > 10) {
-                        JOptionPane.showMessageDialog(null, "Numero de habitacion invalido.");
-                        continue;
+                    // CREAMOS RESERVA LLAMANDO METODO CREARRESERVA
+                    Reservas reserva = crearReserva(hotel, cliente);
+                    if (reserva != null) {
+                        hotel.getReservas().add(reserva); // Añadimos la reserva al hotel
                     }
+                    break;
 
-                    // CREAMOS OBJETO HABITACION
-                    Habitacion habitacionSeleccionada = hotel.getHabitacion()[numeroHabitacion - 1];
-
-                    try {
-                        String fechaStartString = pedirEntrada("Ingrese fecha de comienzo (formato: yyyy/MM/dd):");
-                        if (fechaStartString == null)
-                            return;
-
-                        // SE PARCEA LA FECHA
-                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-                        LocalDate fechaStart = LocalDate.parse(fechaStartString, formatter);
-
-                        // COMPROBAMOS SI LA FECHA NO ES ANTERIOR A LA DE HOY
-                        if (fechaStart.isBefore(LocalDate.now())) {
-                            JOptionPane.showMessageDialog(null,
-                                    "La fecha es incorrecta, no puedes reservar en el pasado");
-                            continue;
-                        }
-
-                        String duracionString = pedirEntrada("Ingrese duracion de su estancia:");
-                        if (duracionString == null)
-                            return;
-
-                        // SE PARCEA STRING A INT
-                        int duracion = Integer.parseInt(duracionString);
-                        Reservas reserva = new Reservas(fechaStart, duracion, cliente, habitacionSeleccionada);
-                        hotel.getReservas().add(reserva);
-
-                    } catch (DateTimeParseException e) {
-                        JOptionPane.showMessageDialog(null, "Has ingresado Mal la fecha");
+                case 2:
+                    // Mostrar las reservas
+                    String reservasList = "";
+                    for (Reservas reservaMostrar : hotel.getReservas()) {
+                        reservasList += reservaMostrar.toString() + "\n";
                     }
+                    JOptionPane.showMessageDialog(null, "Reservas actuales:\n" + reservasList);
+                    break;
 
-                } catch (NumberFormatException e) {
-                    JOptionPane.showMessageDialog(null, "Has ingresado letra en vez de numero de la habitacion");
-                }
+                case 3:
+                    // Salir del programa
+                    JOptionPane.showMessageDialog(null, "¡Gracias por usar el sistema! Adiós.");
+                    return;
 
-                // PARA MOSTRAR LAS RESERVAS
-            } else if (seleccion == 2) {
-                String reservasList = "";
-                for (Reservas reserva : hotel.getReservas()) {
-                    reservasList += reserva.toString() + "\n";
-                }
-                JOptionPane.showMessageDialog(null, "Reservas actuales:\n" + reservasList);
-
-                // PARA SALIR DEL PROGRAMA
-            } else if (seleccion == 3) {
-
-                JOptionPane.showMessageDialog(null, "¡Gracias por usar el sistema! Adiós.");
-                return;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción no válida. Por favor, 1,2 o 3");
+                    break;
             }
         }
+    }
+ 
+    // METODO CREAR CLIENTE
+    public static Cliente crearCliente() {
+
+        String nombre = pedirEntrada("Ingrese el nombre del cliente:");
+        if (nombre == null)
+            return null;
+
+        String apellido = pedirEntrada("Ingrese el apellido del cliente:");
+        if (apellido == null)
+            return null;
+
+        String documento = pedirEntrada("Ingrese el documento del cliente:");
+        if (documento == null)
+            return null;
+
+        // VAMOS A COMPROBAR SI EL DOCUMENTO ES VALIDO
+        String regex = "^\\d{8}[a-zA-Z]$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(documento);
+        if (!matcher.matches()) {
+            JOptionPane.showMessageDialog(null, "El DNI no es válido.");
+            return null;
+        }
+
+        String numeroTarjeta = pedirEntrada("Ingrese el numero de cuenta bancaria (formato: xxxx-xxxx-xxxxxxxx):");
+        if (numeroTarjeta == null)
+            return null;
+
+        // VAMOS A COMPROBAR SI NUMERO DE TARJETA ESTA BIEN
+        regex = "^(\\d{4}[-\\s]?){3}\\d{4}$";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(numeroTarjeta);
+        if (!matcher.matches()) {
+            JOptionPane.showMessageDialog(null, "La cuenta bancaria no está correcta.");
+            return null;
+        }
+
+        // CREAMOS EL OBJETO CLIENTE
+        return new Cliente(nombre, apellido, documento, numeroTarjeta);
+    }
+
+    public static Reservas crearReserva(Hotel hotel, Cliente cliente) {
+        try {
+            String numeroHabitacionString = pedirEntrada("Ingrese el numero de la habitacion (1-10):");
+            if (numeroHabitacionString == null)
+                return null;
+
+            // SE PARCEA STRING A INT
+            int numeroHabitacion = Integer.parseInt(numeroHabitacionString);
+
+            // COMPROBAMOS SI NUMERO DE HABITACION EXISTE
+            if (numeroHabitacion < 1 || numeroHabitacion > 10) {
+                JOptionPane.showMessageDialog(null, "Número de habitación inválido.");
+                return null;
+            }
+
+            // CREAMOS OBJETO HABITACION
+            Habitacion habitacionSeleccionada = hotel.getHabitacion()[numeroHabitacion - 1];
+
+            String fechaStartString = pedirEntrada("Ingrese fecha de comienzo (formato: yyyy/MM/dd):");
+            if (fechaStartString == null)
+                return null;
+
+            // SE PARCEA LA FECHA
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+            LocalDate fechaStart = LocalDate.parse(fechaStartString, formatter);
+
+            // COMPROBAMOS SI LA FECHA NO ES ANTERIOR A LA DE HOY
+            if (fechaStart.isBefore(LocalDate.now())) {
+                JOptionPane.showMessageDialog(null, "La fecha es incorrecta, no puedes reservar en el pasado");
+                return null;
+            }
+
+            String duracionString = pedirEntrada("Ingrese duración de su estancia:");
+            if (duracionString == null)
+                return null;
+
+            // SE PARCEA STRING A INT
+            int duracion = Integer.parseInt(duracionString);
+
+            // CREAMOS Y RETORNAMOS EL OBJETO RESERVA
+            return new Reservas(fechaStart, duracion, cliente, habitacionSeleccionada);
+
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Has ingresado letra en vez de número de la habitación.");
+        } catch (DateTimeParseException e) {
+            JOptionPane.showMessageDialog(null, "Has ingresado mal la fecha.");
+        }
+
+        return null; // En caso de error, retornamos null
     }
 
     public static String pedirEntrada(String mensaje) {
         String input = JOptionPane.showInputDialog(mensaje);
         if (input == null) {
-            JOptionPane.showMessageDialog(null, "Cerrando la super App @Cristobal 2025");
+            JOptionPane.showMessageDialog(null, "Volviendo al menu de la super App @Cristobal 2025");
         }
         return input;
     }
